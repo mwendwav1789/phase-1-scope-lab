d83d4dd843c2fb88cdb602e81685f65be7ae0943
Victor <mwendwav1789@gmail.com>
2024-09-29 21:06:42 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
-// Write your solution in this file!
-// Declare customerName globally
+// Declare a global variable called customerName and assign it the value 'bob'
 var customerName = "bob";
 
-// 1) Function that returns customerName
-function returnCustomerName() {
-  return customerName;
-}
-
-// 2) Function that modifies the global customerName to uppercase
+// Function to uppercase the global customerName variable
 function upperCaseCustomerName() {
-  customerName = customerName.toglobalCase();
+  customerName = customerName.toUpperCase();
 }
 
-// 3) Function that declares and sets bestCustomer
-var bestCustomer;
+// Function that declares a global variable called bestCustomer and assigns it the value 'not bob'
 function setBestCustomer() {
-  bestCustomer = "not bob";
+  bestCustomer = "not bob"; // Implicitly global variable (not recommended in practice)
 }
 
-// 4) Function that overwrites bestCustomer
+// Function to overwrite the value of the global variable bestCustomer
 function overwriteBestCustomer() {
   bestCustomer = "maybe bob";
 }
 
-// 5) Function that tries (unsuccessfully) to reassign leastFavoriteCustomer
-const leastFavoriteCustomer = "Karen";
-function changeLeastFavoriteCustomer() {
-  try {
-    leastFavoriteCustomer = "someone else"; // This will throw an error
-  } catch (error) {
-    console.error("Cannot change least favorite customer: ", error.message);
-  }
-}
+// Declare a constant in global scope called leastFavoriteCustomer and assign an initial value
+const leastFavoriteCustomer = "someone";
 
-// Test the functions
-console.log(returnCustomerName()); // bob
-upperCaseCustomerName();
-console.log(returnCustomerName()); // BOB
-setBestCustomer();
-console.log(bestCustomer); // not bob
-overwriteBestCustomer();
-console.log(bestCustomer); // maybe bob
-changeLeastFavoriteCustomer(); // Error: Cannot change least favorite customer
+// Function that tries to change the constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "anyone"; // This will throw an error because constants cannot be reassigned
+}
